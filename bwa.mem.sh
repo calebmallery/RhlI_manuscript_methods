a8fc86e7daf384f6c7495c92165fc517ad139a64
Caleb Mallery <139981169+calebcodes01@users.noreply.github.com>
2023-08-28 20:22:50 -0400

    Update bwa.mem.sh

--- bwa.mem.sh
+++ bwa.mem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(cat SRR_list)
+for i in $(cat SRRlist.txt)
 do
 bwa mem pa14.fna $i"_1_val_1.fq" $filn"_2_val_2.fq" | samtools sort | samtools view -F 4 -o $filn".sorted.bam"
 done
