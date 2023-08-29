aa376807ff6cb03ba26f5d88d98b61d7cac6835d
Caleb Mallery <139981169+calebcodes01@users.noreply.github.com>
2023-08-28 21:00:02 -0400

    Update and rename bwa.mem.sh to many_bwa.mem.sh

--- many_bwa.mem.sh
+++ many_bwa.mem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(cat SRRlist.txt)
+for i in $(cat SRRaccessions.txt)
 do
 bwa mem pa14.fna $i"_1_val_1.fq" $i"_2_val_2.fq" | samtools sort | samtools view -F 4 -o $i".sorted.bam"
 done
