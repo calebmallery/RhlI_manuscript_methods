cade798e99ea2ac9dd3b43a7fd489b8278a241fb
Caleb Mallery <139981169+calebcodes01@users.noreply.github.com>
2023-08-28 20:19:05 -0400

    Update bwa.mem.sh

--- bwa.mem.sh
+++ bwa.mem.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-for i in $(find ./ -type f -name "*.fastq.gz" | while read F; do basename $F | rev | cut -c 22- | rev; done | sort | uniq)
-
-    do echo "Merging R1"
-
-cat "$i"_L00*_R1_001.fastq.gz > "$i"_R1.fastq.gz
-
-       echo "Merging R2"
-
-cat "$i"_L00*_R2_001.fastq.gz > "$i"_R2.fastq.gz
-
-done;
+for i in $(cat SRR_list)
+do
+bwa mem pa14.fna $i"_1_val_1.fq" $filn"_2_val_2.fq" | samtools sort | samtools view -F 4 -o $filn".sorted.bam"
+done
